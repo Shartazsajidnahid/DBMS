3bdad464024e650c48ae5c06127ab32bd10747d0
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2022-03-16 15:10:32 +0600

    Update decisionTree.py

--- decision tree/decisionTree.py
+++ decision tree/decisionTree.py
@@ -1,28 +1,23 @@
-import splitting
+import splitter as SP
 
 class DecisionTree:
-    def __init__(self, rows):
+    def __init__(self, rows, div_dim, div_val):
         self.data = rows
-        leftChild, rightChild = None, None
+        self.dim, self.val = div_dim, div_val
+        self.leftChild, self.rightChild= None, None
 
 def build_decision_tree(parent, n_class):
-    best_dim, best_val= splitting.select_split(parent.data, n_class)
-    left, right= splitting.split(best_dim, best_val, parent.data)
-    
-    if(splitting.entropy(left, n_class)!=0):
+    best_dim, best_val= SP.select_split(parent, n_class)
+    left, right= SP.split_rows(best_dim, best_val, parent)
+    leftNode= DecisionTree(left, 0, 0.0)
+    rightNode= DecisionTree(right, 0, 0.0)
+    if SP.entropy(left, n_class)!=0 and SP.get_tot_rows(left)>1:
         leftNode = build_decision_tree(left, n_class)
-    if(splitting.entropy(right, n_class)!=0):
+    if SP.entropy(right, n_class)!=0 and SP.get_tot_rows(right)>1:
         rightNode = build_decision_tree(right, n_class)
 
-    parentNode = DecisionTree(parent)
+    parentNode = DecisionTree(parent, best_dim, best_val)
     parentNode.leftChild= leftNode
     parentNode.rightChild= rightNode
     return parentNode
-
-
-
-root = build_decision_tree(dataset, n_class)
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Sec
+    
