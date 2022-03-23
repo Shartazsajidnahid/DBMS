eaf3204f64d29d54b71f40baf847aa12d24e55e9
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2022-03-23 15:22:59 +0600

    Update knn.py

--- knn.py
+++ knn.py
@@ -7,47 +7,52 @@ from math import *
 
 import glob
 
-sunfile = glob.glob("/home/nahid/Downloads/sun/*")
-moonfile = glob.glob("/home/nahid/Downloads/moon/*")
+sunfile = glob.glob("/home/nahid/Documents/desert/*")
+moonfile = glob.glob("/home/nahid/Documents/Snow/*")
 
-newimg = Image.open("/home/nahid/Downloads/night.jpeg")
-img  = Image.open("/home/nahid/Downloads/sun/sun1.jpeg")
+
+newimg = Image.open("/home/nahid/Documents/6.jpg")
+
+#img  = Image
+newimg.resize((200,200))
+
+# .open("/home/nahid/Downloads/sun/sun1.jpeg")
+
 
 
 newa = asarray(newimg)
 newa = np.reshape(newa, (np.product(newa.shape),)) 
 
-def getmin(a,b):
-    if(a>b):
-         return b
-    else:
-         return a
-    
+
 dr = list()
 for file in sunfile: 
     img = Image.open(file)
+    img.resize((200,200))
     suna = asarray(img)
     suna = np.reshape(suna, (np.product(suna.shape),))
-    x = getmin(len(suna), len(newa))
+    #x = getmin(len(suna), len(newa))
     dis = 0
-    for i in range(x):
+    for i in range(400):
         dis += (newa[i] - suna[i])**2
     dis = sqrt(dis)
-    dr.append([dis,"sun"])
+    dr.append([dis,"desert"])
+
+   
 
 
 for file in moonfile: 
     img = Image.open(file)
+    img.resize((200,200))
+    
     moona = asarray(img)
     moona = np.reshape(moona, (np.product(moona.shape),))
-    x = getmin(len(moona), len(newa))
+    
+    #x = getmin(len(moona), len(newa))
     dis = 0
-    for i in range(x):
+    for i in range(400):
         dis += (newa[i] - moona[i])**2
     dis = sqrt(dis)
-    dr.append([dis,"moon"])
-
-
+    dr.append([dis,"snow"])
 
 
 dr = sorted(dr, key=lambda x: x[0])
@@ -55,6 +60,10 @@ for dis in dr:
     print(dis)
 
 k = 3
-for i in range(k):
-    
+for dis in dr:
+    print(dis[1])
+    k-=1
+    if(k==0):
+        break
+
 # print(dr)
