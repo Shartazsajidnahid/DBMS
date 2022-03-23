441e11ca2c13e6c4df1eba19a6a1181db9e32bff
Shartazsajidnahid <48786813+Shartazsajidnahid@users.noreply.github.com>
2022-03-23 23:33:15 +0600

    Update and rename knn.py to KNN/knn.py

--- KNN/knn.py
+++ KNN/knn.py
@@ -7,11 +7,12 @@ from math import *
 
 import glob
 
-sunfile = glob.glob("/home/nahid/Documents/desert/*")
-moonfile = glob.glob("/home/nahid/Documents/Snow/*")
+sunfile = glob.glob("/home/nahid/Downloads/sun/*")
+moonfile = glob.glob("/home/nahid/Downloads/moon/*")
 
 
-newimg = Image.open("/home/nahid/Documents/6.jpg")
+newimg = Image.open("/home/nahid/Downloads/newmoon.jpeg")
+newimg.show()
 
 #img  = Image
 newimg.resize((200,200))
@@ -28,31 +29,32 @@ dr = list()
 for file in sunfile: 
     img = Image.open(file)
     img.resize((200,200))
-    suna = asarray(img)
-    suna = np.reshape(suna, (np.product(suna.shape),))
+    sunimg = img.getdata()
+    suna = np.array(sunimg)
+    suna = suna.flatten()
     #x = getmin(len(suna), len(newa))
     dis = 0
     for i in range(400):
         dis += (newa[i] - suna[i])**2
     dis = sqrt(dis)
-    dr.append([dis,"desert"])
+    dr.append([dis,"sun"])
 
-   
-
+  
 
 for file in moonfile: 
     img = Image.open(file)
     img.resize((200,200))
     
-    moona = asarray(img)
-    moona = np.reshape(moona, (np.product(moona.shape),))
+    moonimg = img.getdata()
+    moona = np.array(moonimg)
+    moona = moona.flatten()
     
     #x = getmin(len(moona), len(newa))
     dis = 0
     for i in range(400):
         dis += (newa[i] - moona[i])**2
     dis = sqrt(dis)
-    dr.append([dis,"snow"])
+    dr.append([dis,"moon"])
 
 
 dr = sorted(dr, key=lambda x: x[0])
@@ -60,10 +62,23 @@ for dis in dr:
     print(dis)
 
 k = 3
+n_sun = 0
+n_moon = 0
+
 for dis in dr:
-    print(dis[1])
+    #print(dis[1])
+    if(dis[1]=='sun'):
+        n_sun+=1
+    else:
+        n_moon+=1
     k-=1
     if(k==0):
         break
+
+if(n_sun>n_moon):
+    print("The image is a sun type")
+else: 
+    print("The image is a moon type")
+
 
 # print(dr)
